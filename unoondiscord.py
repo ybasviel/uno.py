eadb16a630e4b6ae549f40ff85f083418a5b40cb
ybasviel <calin.8082@gmail.com>
2020-12-06 17:55:14 +0900

    add uno

--- unoondiscord.py
+++ unoondiscord.py
@@ -1,4 +1,6 @@
 import discord
+import random
+import time
 
 # 自分のBotのアクセストークン
 with open("bot.token") as tokenfile:
@@ -46,7 +48,7 @@ async def on_message(message):
 
             error = False
             for x in range(len(roommemberid)):
-                if message.authorid in x:
+                if message.author.id in x:
                     error = True
             
             if error:
@@ -130,7 +132,256 @@ async def on_message(message):
                 await message.channel.send(roomlist[-1] + "を作成しました！")
 
 
+        if command.startswith("start"):
+            authorid = message.author.id
+            roomnum = -1
+            usernum = -1
+
+            for x in range(len(roommemberid)):
+                for y in range(len(roommemberid[x])):
+                    if authorid == roommemberid[x][y]:
+                        roomnum = x
+                        usernum = y
+            
+
+            if roomnum == -1:
+                await message.channel.send("あなたはルームに参加していません")
+            elif usernum != 0:
+                await message.channel.send("あなたはホストではありません")
+            else:
+                await uno(roomlist[roomnum],roommemberlist[roomnum],roommemberid[roomnum])
+
+@client.event
+async def uno(roomname,membersname,membersid):
+    yama = ["r0","r1","r1","r2","r2","r3","r3","r4","r4","r5","r5","r6","r6","r7","r7","r8","r8","r9","r9"]
+    yama += ["b0","b1","b1","b2","b2","b3","b3","b4","b4","b5","b5","b6","b6","b7","b7","b8","b8","b9","b9"]
+    yama += ["y0","y1","y1","y2","y2","y3","y3","y4","y4","y5","y5","y6","y6","y7","y7","y8","y8","y9","y9"]
+    yama += ["g0","g1","g1","g2","g2","g3","g3","g4","g4","g5","g5","g6","g6","g7","g7","g8","g8","g9","g9"]
+    yama += ["Wx","Wx","Wx","Wx"]
+    yama += ["rD","rD","bD","bD","yD","yD","gD","gD"]
+    yama += ["rR","rR","bR","bR","yR","yR","gR","gR"]
+    yama += ["rS","rS","bS","bS","yS","yS","gS","gS"]
+    random.shuffle(yama)
+
+    playercard = []
+    population = len(membersid)
+
+    for x in range(len(membersid)):
+        playercard.append([])
+
+    for i in range(7):
+        for x in range(population):
+            playercard[x].append(yama.pop())
+
+    for x in range(population):
+        player = client.get_user(membersid[x])
+        allplayers = ""
+        for y in range(population):
+            if y == x:
+                pass
+            else:
+                allplayers = "と" + membersname[y] + " さん "
         
+        await player.send("ゲームの準備ができました！\n対戦相手は " + allplayers[1:] + "です")
+    
+    time.sleep(1.5)
+
+    ba = yama.pop()
+    while ba == "Wx":
+        yama.append(ba)
+        random.shuffle(yama)
+        ba = yama.pop()
+
+    daseru = []
+
+    drawnext = 0
+    next = 1
+
+    turnnum = random.randrange(population)
+
+    for x in range(population):
+        player = client.get_user(membersid[x])
+
+        if x == turnnum:
+            await player.send("あなたからスタートです")
+        else:
+            await player.send( membersname[turnnum] + "さんからスタートです")
+
+    game = True
+    while game: #ゲームスタート
+
+        nowplayerid = membersid[turnnum%population]
+        player = client.get_user(nowplayerid)
+        
+        for i in range(drawnext):
+            playercard[turnnum%population].append(yama.pop())
+    
+        drawnext = 0
+
+        daseru = []
+
+        tefudanum = len(playercard[turnnum%population])
+
+        sendtext = "    場にあるのは\n" + ba + "です．どれを捨てますか(番号で選択)\n"
+
+        for x in range(tefudanum):
+            tefudainfo = playercard[turnnum%population][x]
+            if ba[0] == tefudainfo[0] or ba[1] == tefudainfo[1] or "x" == tefudainfo[1]:
+                sendtext += "[" + str(x) + "]:" + playercard[turnnum%population][x] + "  "
+                daseru.append(x)
+            else:
+                sendtext += "[E]:" + playercard[turnnum%population][x] + "  "
+
+        if len(daseru) == 0:
+            playercard[turnnum%population].append(yama.pop())
+            sendtext += "\n    出せる手札がないのでパスします．\n    引いた手札は"
+
+            tefudainfo = playercard[turnnum%population][-1]
+            sendtext += playercard[turnnum%population][tefudanum] + "です．"
+
+            embed = discord.Embed(title="あなたのターンです",description=sendtext,color=discord.Colour.from_rgb(255,0,0))
+            await player.send(embed=embed)
+
+        else:
+
+            embed = discord.Embed(title="あなたのターンです",description=sendtext,color=discord.Colour.from_rgb(255,0,0))
+            await player.send(embed=embed)
+            sendtext = ""
+
+            def cardcheck(message):
+                return message.author.id == nowplayerid and int(message.content) in daseru
+
+            msg = await client.wait_for('message', check=cardcheck)
+            suteru = msg.content
+            suteru = int(suteru)
+
+            suteruinfo = playercard[turnnum%population][int(suteru)]
+
+            if suteruinfo[0] == "W":
+
+                await player.send("どの色にしますか(数字で選択)\n[0]:red [1]:blue [2]:yellow [3]:green")
+                
+                def irocheck(message):
+                    return message.author.id == nowplayerid and message.content in ["0","1","2","3"]
+
+                msg = await client.wait_for('message', check=irocheck)
+                iro = msg.content
+                iro = int(iro)
+
+                wildcolors = ["rx","bx","yx","gx"]
+                playercard[turnnum%population][suteru] = wildcolors[iro]
+
+            elif suteruinfo[1] == "D":
+                drawnext = 2
+                sendtext = "ドロー +" + str(drawnext) + "！"
+            elif suteruinfo[1] == "R":
+                next = -next
+                sendtext = "リバース！"
+            elif suteruinfo[1] == "S":
+                turnnum += next
+                sendtext = "スキップ！"
+
+            yama.insert(0,ba)
+            ba = playercard[turnnum%population][suteru]
+            del playercard[turnnum%population][suteru]
+
+            #連続投下
+
+            daseru = []
+
+            tefudanum = len(playercard[turnnum%population])
+
+            for x in range(tefudanum):
+                tefudainfo = playercard[turnnum%population][x]
+                if ba[1:] == tefudainfo[1:]:
+                    daseru.append(x)
+
+
+
+            while len(daseru) != 0:
+
+                sendtext += "\n  あなたが捨てたのは" + ba + "です．続けて捨てますか(番号で選択)\n"
+                for x in range(tefudanum):
+                    tefudainfo = playercard[turnnum%population][x]
+                    if ba[1] == tefudainfo[1]:
+                        sendtext += "[" + str(x) + "]:" + playercard[turnnum%population][x] + "  "
+                        #daseru.append(x)
+                    else:
+                        sendtext += "[E]:" + playercard[turnnum%population][x] + "  "
+
+                sendtext += "[Q]:捨てない"
+
+                embed = discord.Embed(title="",description=sendtext,color=discord.Colour.from_rgb(255,0,0))
+                await player.send(embed=embed)
+
+                msg = await client.wait_for('message', check=cardcheck)
+                suteru = msg.content
+
+                if suteru[0] != "Q" and suteru[0] != "q":
+
+                    suteru = int(suteru)
+                    suteruinfo = playercard[turnnum%population][suteru]
+
+                    if suteruinfo[0] == "W":
+
+                        await player.send("どの色にしますか(数字で選択)\n[0]:red [1]:blue [2]:yellow [3]:green")
+
+                        def irocheck(message):
+                            return message.author.id == nowplayerid and message.content in ["0","1","2","3"]
+
+                        msg = await client.wait_for('message', check=irocheck)
+                        iro = msg.content
+                        iro = int(iro)
+
+                        wildcolors = ["rx","bx","yx","gx"]
+                        playercard[turnnum%population][suteru] = wildcolors[iro]
+
+                    elif suteruinfo[1] == "D":
+                        drawnext = 2
+                        sendtext = "ドロー +" + str(drawnext) + "！"
+                    elif suteruinfo[1] == "R":
+                        next = -next
+                        sendtext = "リバース！"
+                    elif suteruinfo[1] == "S":
+                        turnnum += next
+                        sendtext = "スキップ！"
+
+
+                    #まだ出せるかチェック
+                    yama.insert(0,ba)
+                    ba = playercard[turnnum%population][suteru]
+                    del playercard[turnnum%population][suteru]
+
+                    daseru = []
+
+                    tefudanum = len(playercard[turnnum%population])
+
+                    for x in range(tefudanum):
+                        tefudainfo = playercard[turnnum%population][x]
+                        if ba[1] == tefudainfo[1]:
+                            daseru.append(x)
+
+
+                else:
+                    daseru = []
+
+            sendtext += "\n  あなたが捨てたのは" + ba + "です．"
+            await player.send(sendtext)
+
+        for x in range(population):
+            if playercard[x] == []:
+                game = False
+
+        turnnum += next
+
+    #終了メッセージ
+    for x in range(population):
+        player = client.get_user(membersid[x])
+
+        if x == turnnum:
+            await player.send("あなたの勝ちです")
+        else:
+            await player.send( membersname[turnnum] + "さん の勝ちです")
 
 
 
